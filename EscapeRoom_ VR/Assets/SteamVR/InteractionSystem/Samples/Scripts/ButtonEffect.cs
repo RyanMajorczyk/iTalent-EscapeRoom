38997b35066d4321f7649b64492a458356a19f57
MatthiasVDB <matthias.vandenbergh@student.pxl.be>
2018-10-31 15:34:56 +0100

    On LightSwitch Pressed Script Update

--- EscapeRoom_ VR/Assets/SteamVR/InteractionSystem/Samples/Scripts/ButtonEffect.cs
+++ EscapeRoom_ VR/Assets/SteamVR/InteractionSystem/Samples/Scripts/ButtonEffect.cs
@@ -9,9 +9,35 @@ namespace Valve.VR.InteractionSystem.Sample
 {
     public class ButtonEffect : MonoBehaviour
     {
+        public Light light1;
+        public Light light2;
+        public Light light3;
+        public GameObject lamp1;
+        public GameObject lamp2;
+        public GameObject lamp3;
 
         public void OnButtonDown(Hand fromHand)
         {
+            if (light1 != null)
+            {
+                light1.enabled = !light1.enabled;
+                light2.enabled = !light2.enabled;
+                light3.enabled = !light3.enabled;
+                if (!light1.enabled)
+                {
+                    lamp1.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+                    lamp2.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+                    lamp3.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+                }
+                else
+                {
+                    lamp1.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+                    lamp2.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+                    lamp3.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+                }
+            }
+            
+
             ColorSelf(Color.cyan);
             fromHand.TriggerHapticPulse(1000);
         }
