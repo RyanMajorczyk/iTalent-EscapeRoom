884de60d557bdefa0db6118aeb174d236c1737ee
MatthiasVDB <matthias.vandenbergh@student.pxl.be>
2018-10-31 15:11:12 +0100

    Namespace change LightSwitch Script

--- EscapeRoom_ VR/Assets/LightSwitch.cs
+++ EscapeRoom_ VR/Assets/LightSwitch.cs
@@ -1,33 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR.InteractionSystem;
 
-public class LightSwitch : MonoBehaviour {
-
-    public Light light1;
-    public Light light2;
-    public Light light3;
-    public GameObject lamp1;
-    public GameObject lamp2;
-    public GameObject lamp3;
-
-    public void OnButtonDown(Hand fromHand)
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class LightSwitch : MonoBehaviour
     {
-        light1.enabled = !light1.enabled;
-        light2.enabled = !light2.enabled;
-        light3.enabled = !light3.enabled;
-        if (!light1.enabled)
+
+        public Light light1;
+        public Light light2;
+        public Light light3;
+        public GameObject lamp1;
+        public GameObject lamp2;
+        public GameObject lamp3;
+
+        public void OnButtonDown(Hand fromHand)
         {
-            lamp1.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
-            lamp2.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
-            lamp3.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
-        }
-        else
-        {
-            lamp1.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
-            lamp2.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
-            lamp3.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+            light1.enabled = !light1.enabled;
+            light2.enabled = !light2.enabled;
+            light3.enabled = !light3.enabled;
+            if (!light1.enabled)
+            {
+                lamp1.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+                lamp2.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+                lamp3.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+            }
+            else
+            {
+                lamp1.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+                lamp2.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+                lamp3.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+            }
         }
     }
 }
