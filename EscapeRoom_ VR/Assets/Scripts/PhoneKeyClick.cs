2507d8d2506269def50f4a419e581e2442226401
Jana Giebens <jana.giebens@student.pxl.be>
2018-11-07 15:12:58 +0100

    Test push phone buttons

---
+++ EscapeRoom_ VR/Assets/Scripts/PhoneKeyClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class PhoneKeyClick : MonoBehaviour
+    {
+        public string value;
+
+        public void OnButtonDown(Hand fromHand)
+        {
+            Debug.Log("Button pressed " + value);
+        }
+    }
+}
