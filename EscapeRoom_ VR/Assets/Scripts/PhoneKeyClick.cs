bb2cc9e218b2293b33f3f89d17fd902e7c08c981
Jana Giebens <jana.giebens@student.pxl.be>
2018-11-27 09:25:27 +0100

    Updated phoneKeyClick script

--- EscapeRoom_ VR/Assets/Scripts/PhoneKeyClick.cs
+++ EscapeRoom_ VR/Assets/Scripts/PhoneKeyClick.cs
@@ -8,8 +8,24 @@ namespace Valve.VR.InteractionSystem.Sample
     {
         public string value;
 
+        public static string insertedPhoneNumber = "";
+
         public void OnButtonDown(Hand fromHand)
         {
+            if (value.Equals("-1"))
+            {
+                //Check if number is correct and call cellphone
+            }
+            else if (value.Equals("-2"))
+            {
+                insertedPhoneNumber = "";
+                Debug.Log("Cancel button Number is: " + insertedPhoneNumber);
+            }
+            else
+            {
+                insertedPhoneNumber += value;
+                Debug.Log(insertedPhoneNumber);
+            }
             Debug.Log("Button pressed " + value);
         }
     }
