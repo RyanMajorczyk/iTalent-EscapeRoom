e05e5fdb69da6e7acb46fba59e1713820078ed0f
MatthiasVDB <matthias.vandenbergh@student.pxl.be>
2018-12-03 19:59:59 +0100

    Update smartphone sound and emission on call

--- EscapeRoom_ VR/Assets/Scripts/PhoneKeyClick.cs
+++ EscapeRoom_ VR/Assets/Scripts/PhoneKeyClick.cs
@@ -17,6 +17,8 @@ namespace Valve.VR.InteractionSystem.Sample
 
         private Renderer _renderer;
         private AudioSource _ringtone;
+        private Color _finalValue;
+        private Material _material;
 
         public Material _blackScreen;
         public Material _callScreen;
@@ -25,10 +27,18 @@ namespace Valve.VR.InteractionSystem.Sample
         {
             _renderer = _smartphone.GetComponent<Renderer>();
             _ringtone = _smartphone.GetComponent<AudioSource>();
-            Material material = _renderer.materials[1];
-            Color finalValue = Color.black;
-            //Color finalValue = Color.black * 0.1f;
-            material.SetColor("_EmissionColor", finalValue);
+            _material = _renderer.materials[1];
+            _finalValue = Color.black;
+            //_finalValue = Color.black * 0.1f;
+            _material.SetColor("_EmissionColor", _finalValue);
+        }
+
+        void Update()
+        {
+            if (!_ringtone.isPlaying)
+            {
+                _material.SetColor("_EmissionColor", _finalValue);
+            }
         }
 
         public void OnButtonDown(Hand fromHand)
@@ -40,11 +50,12 @@ namespace Valve.VR.InteractionSystem.Sample
                     if (insertedPhoneNumber.Equals(correctPhoneNumber))
                     {
                         //Make phone call
-                        Material material = _renderer.materials[1];
-                        Color finalValue = Color.white;
-                        //Color finalValue = Color.white * 0.1f;
-                        material.SetColor("_EmissionColor", finalValue);
-                        _ringtone.mute = false;
+                        //_material = _renderer.materials[1];
+                        _finalValue = Color.white * 2.0f;
+                        //_finalValue = Color.white * 0.1f;
+                        _material.SetColor("_EmissionColor", _finalValue);
+                        _ringtone.Play();
+                        _finalValue = Color.black;
                     }
 
                 }
