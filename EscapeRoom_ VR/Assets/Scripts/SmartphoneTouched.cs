fa175794929f2acbb3102d38355a0b5e93c08009
MatthiasVDB <matthias.vandenbergh@student.pxl.be>
2018-12-03 11:27:26 +0100

    Added ringtone to smartphone

---
+++ EscapeRoom_ VR/Assets/Scripts/SmartphoneTouched.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartphoneTouched : MonoBehaviour {
+
+    Vector3 touchPosWorld;
+
+    //Change me to change the touch phase used.
+    TouchPhase touchPhase = TouchPhase.Began;
+
+    // Update is called once per frame
+    void Update () {
+        //We check if we have more than one touch happening.
+        //We also check if the first touches phase is Ended (that the finger was lifted)
+        if (Input.GetTouch(0).phase == touchPhase)
+        {
+            //We transform the touch position into word space from screen space and store it.
+            touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+            Vector3 touchPosWorld3D = new Vector3(touchPosWorld.x, touchPosWorld.y, touchPosWorld.z);
+
+            //We now raycast with this information. If we have hit something we can process it.
+            RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld3D, Camera.main.transform.forward);
+
+            if (hitInformation.collider != null)
+            {
+                //We should have hit something with a 2D Physics collider!
+                GameObject touchedObject = hitInformation.transform.gameObject;
+
+                touchedObject.GetComponent<AudioSource>().mute = true;
+                //touchedObject should be the object someone touched.
+                Debug.Log("Touched " + touchedObject.transform.name);
+            }
+        }
+    }
+}
