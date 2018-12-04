3a667e383bb64b5120114ad9c8871b7e6e5161ba
RyanMajorczyk <ryan.majorczyk@student.pxl.be>
2018-12-04 09:45:14 +0100

    Adding door to mainScene

--- EscapeRoom_ VR/Assets/Scripts/KeyTrigger.cs
+++ EscapeRoom_ VR/Assets/Scripts/KeyTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyTrigger : MonoBehaviour {
-
+    public GameObject doorToOpen;
 	// Use this for initialization
 	void Start () {
 		
@@ -19,6 +19,7 @@ public class KeyTrigger : MonoBehaviour {
         if (other.gameObject.tag == "key")
         {
             Debug.Log("The door can be opened");
+            doorToOpen.transform.Rotate(0, 90, 0);
         }
         else
         {
