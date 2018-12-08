124ac9d27af6306509bef3a17199c900d6689f13
RyanMajorczyk <ryan.majorczyk@student.pxl.be>
2018-12-08 20:49:36 +0100

    Finished room (Maybe)

---
+++ EscapeRoom_ VR/Assets/Scripts/ObjectPuller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPuller : MonoBehaviour {
+
+    public float pullRadius = 2;
+    public float pullForce = 1;
+
+    public void FixedUpdate()
+    {
+        foreach (Collider collider in Physics.OverlapSphere(transform.position, pullRadius))
+        {
+            Vector3 forceDirection = transform.position - collider.transform.position;
+
+            if (collider.gameObject.tag == "key")
+            {
+                collider.GetComponent<Rigidbody>().AddForce(forceDirection.normalized * pullForce * Time.fixedDeltaTime);
+            }
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FixedUpdate();
+	}
+}
